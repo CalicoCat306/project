72fdbe20b4876a68624f79d524573a9dfbc6a487
CalicoCat306 <89363183+CalicoCat306@users.noreply.github.com>
2023-10-15 17:17:48 +0900

    Update q2.go

--- q2.go
+++ q2.go
@@ -40,32 +40,9 @@ func sum(num int, fileName string) int {
 	checkError(err)
 
 	// Create buffered channel
-	intsBuffer := len(ints) / num  // Buffer size. 파이썬과 다르게 몫을 반환
-	sumChan := make(chan int, num) // 부분합들을 저장할 채널
+	
 
-	// Create workers
-	for i := 0; i < num; i++ {
-		intsChan := make(chan int, intsBuffer)
-		for j := 0; j < intsBuffer; j++ {
-			intsChan <- ints[i*intsBuffer+j]
-		}
-		go sumWorker(intsChan, sumChan)
-
-		// Close channel
-		close(intsChan)
-	}
-
-	// Sum integers
-	sum := 0
-	for i := 0; i < num; i++ {
-		sum += <-sumChan
-		// fmt.Println("sum :", sum)
-	}
-
-	// Close channel
-	close(sumChan)
-
-	return sum
+	return 1
 }
 
 // Read a list of integers separated by whitespace from `r`.
