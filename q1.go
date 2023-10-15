1865fc9a4e2f661c2a823fe7342cb8a6ec817faf
CalicoCat306 <89363183+CalicoCat306@users.noreply.github.com>
2023-10-15 18:45:35 +0900

    Update q1.go

--- q1.go
+++ q1.go
@@ -2,26 +2,43 @@ package ds_hw_0
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
 	"sort"
+	"strings"
 )
 
-// Find the top K most common words in a text document.
-// 	path: location of the document
-//	numWords: number of words to return (i.e. k)
-//	charThreshold: character threshold for whether a token qualifies as a word,
-//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
-// Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
-// A word comprises alphanumeric characters only. All punctuations and other characters
-// are removed, e.g. "don't" becomes "dont".
-// You should use `checkError` to handle potential errors.
 func topWords(path string, numWords int, charThreshold int) []WordCount {
-	// TODO: implement me
-	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
-	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
-	return nil
+	fileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wordCounts := make(map[string]int)
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+
+	for _, word := range strings.Fields(string(fileBytes)) {
+		word = strings.ToLower(word)
+		word = reg.ReplaceAllString(word, "")
+		if len(word) >= charThreshold {
+			wordCounts[word]++
+		}
+	}
+
+	var wc []WordCount
+	for k, v := range wordCounts {
+		wc = append(wc, WordCount{k, v})
+	}
+	sortWordCounts(wc)
+
+	if len(wc) < numWords {
+		numWords = len(wc)
+	}
+
+	return wc[:numWords]
 }
 
-// A struct that represents how many times a word is observed in a document
 type WordCount struct {
 	Word  string
 	Count int
@@ -31,9 +48,6 @@ func (wc WordCount) String() string {
 	return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
 }
 
-// Helper function to sort a list of word counts in place.
-// This sorts by the count in decreasing order, breaking ties using the word.
-// DO NOT MODIFY THIS FUNCTION!
 func sortWordCounts(wordCounts []WordCount) {
 	sort.Slice(wordCounts, func(i, j int) bool {
 		wc1 := wordCounts[i]
