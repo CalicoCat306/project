b2e506a20622388d5e55bae0d7b8b320cf6a2c2e
CalicoCat306 <89363183+CalicoCat306@users.noreply.github.com>
2023-10-16 09:45:42 +0900

    Update q1_test.go

--- q1_test.go
+++ q1_test.go
@@ -24,7 +24,7 @@ func assertEqual(t *testing.T, answer, expected []WordCount) {
 			expected,
 			answer))
 	} else {
-		fmt.Println("Pass!!")
+		fmt.Sprintf("Expected and Actual : %v\nPass!", answer)
 	}
 }
 
